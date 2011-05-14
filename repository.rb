6ed52f0b137b4c43e58ba49dd6712d77ddb14296
Michael Feathers <mfeathers@acm.org>
2011-05-14 09:17:56 -0400

    added commit collection to repository

--- repository.rb
+++ repository.rb
@@ -4,8 +4,13 @@ module RepoDepot
     attr_reader :classes
 
     def initialize name, events
+      @events = events
       @classes = events.group_by {|e| e.class_name }
                        .map { |name, events| Class.new(name, events) }
+    end
+
+    def commits
+      @commits ||= @events.map { |e| e.commit }
     end
   end
 end
--- repository_spec.rb
+++ repository_spec.rb
@@ -15,5 +15,14 @@ describe "RepoDepot::Repository" do
     repository.classes.count.should == 1
   end
 
+  it "should generate no commits when there are no events" do
+    RepoDepot::Repository.new('example', []).commits.should be_empty
+  end
 
+  it "should create a list of commits for events" do
+    events = [CodeEvent.new(commit: '9e9273dcbbc7bcc882520f2a8ffe13e4f3b273ac'),
+              CodeEvent.new(commit: 'e9c1c0adb4e92d4b2c4117dbc139821ccf2b2851')]
+
+
+  end
 end
