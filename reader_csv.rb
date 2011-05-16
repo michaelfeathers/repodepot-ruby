4b5d8b0895cee7a9bffaed2f4e391d9c866186f9
coreyhaines <coreyhaines@gmail.com>
2011-05-16 10:51:22 -0400

    Include read_events csv reader

--- reader_csv.rb
+++ reader_csv.rb
@@ -1,25 +1,30 @@
-def read_events file_name
- first_row = true
- names = []
- events = []
- CSV.foreach(file_name + ".csv") do |row|
-   if first_row
-     names = row
-     first_row = false
-   else
-     event_hash = {}
-     row.zip((0..(row.size - 1))).each do |field,position|
-       if names[position] == "date"
-         event_hash[names[position]] = Time.parse(field)
-       elsif names[position] == "complexity"
-         event_hash[names[position]] = field.to_f
-       else
-         event_hash[names[position]] = field
-       end
-     end
-     events << CodeEvent.new(event_hash)
-   end
- end
- events
-end
+module Readers
+  module CSV
+    def read_events file_name
+      first_row = true
+      names = []
+      events = []
+      CSV.foreach(file_name + ".csv") do |row|
+        if first_row
+          names = row
+          first_row = false
+        else
+          event_hash = {}
+          row.zip((0..(row.size - 1))).each do |field,position|
+            field_name = names[position]
+            if field_name == "date"
+              event_hash[field_name] = Time.parse(field)
+            elsif field_name == "complexity"
+              event_hash[field_name] = field.to_f
+            else
+              event_hash[field_name] = field
+            end
+          end
+          events << CodeEvent.new(event_hash)
+        end
+      end
+      events
+    end
 
+  end
+end
