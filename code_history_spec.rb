343d18918f11d11b154c3fafb4bb86d07ec1014b
Michael Feathers <mfeathers@acm.org>
2011-05-18 07:16:13 -0400

    added spec for CodeHistory#for_commit

---
+++ code_history_spec.rb
@@ -0,0 +1,17 @@
+
+require 'rspec'
+require_relative 'code_event.rb'
+require_relative 'code_history.rb'
+
+describe "RepoDepot::CodeHistory" do
+  context "#for_commit" do
+    it "returns a new repository for a commit" do
+      commit = 'a1b2c3'
+      event = CodeEvent.new(commit: commit, method_name: 'foo')
+      repository = RepoDepot::CodeHistory.new('example', [event]).for_commit commit
+      repository.methods.count.should eq(1)
+      repository.classes.count.should eq(1)
+    end
+  end
+
+end
