af7934f0aabff28b6c41d946dfeca8c861383ef3
Michael Feathers <mfeathers@acm.org>
2011-05-21 13:19:25 -0400

    added another test for #state_at_commit

--- code_history_spec.rb
+++ code_history_spec.rb
@@ -17,10 +17,27 @@ describe "RepoDepot::CodeHistory" do
     it "finds an event at a commit" do
       commit = 'a1b2c3'
       event = CodeEvent.new(commit: commit, method_name: 'bar')
-      repository = RepoDepot::CodeHistory.new('example', [event]).for_commit commit
-      repository.state_at_commit(commit).first.method_name.should eq('bar')
-      repository.state_at_commit(commit).count.should eq(1)
+      repository = RepoDepot::CodeHistory.new('example', [event]).for_commit(commit)
+      repository.state_at_commit(commit).map {|e| e.method_name }.should == ["bar"]
     end
+
+    it "finds two events at a commit" do
+      events = []
+      commit = 'a'
+      events << CodeEvent.new(commit: commit, class_name: 'Foo', method_name: 'bar')
+      events << CodeEvent.new(commit: commit, class_name: 'Foo', method_name: 'baz')
+      repository = RepoDepot::CodeHistory.new('example', events).for_commit(commit)
+      repository.state_at_commit(commit).map {|e| e.method_name }.should == ["bar", "baz"]
+    end
+
+    # it "finds an event that is earlier than the commit if there is no event for the commit" do
+    #  events = []
+    #  events << CodeEvent.new(commit: 'a', class_name: 'Foo', method_name: 'bar', date: '1/1/2011')
+    #  events << CodeEvent.new(commit: 'b', class_name: 'Foo', method_name: 'baz', date: '1/1/2012')
+    #  repository = RepoDepot::CodeHistory.new('example', events).for_commit('b')
+    #  repository.state_at_commit('b').map {|e| e.method_name }.should == ["bar", "baz"]
+    # end
   end
+
 
 end
